c5d77ded397d28cbf1298ee7abc9e177ec140045
mark390 <55713492+mark390@users.noreply.github.com>
2021-05-04 21:58:33 -0400

    Update Database.php

--- config/Database.php
+++ config/Database.php
@@ -1,15 +1,15 @@
 <?php
     class Database {
-        private $host = 'localhost';
-        private $dbname = 'quotesdb';
-        private $username = 'root';
-        //private $password = ''
+        private $host = 'wcwimj6zu5aaddlj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+        private $dbname = 'y1y12yxrz3q0ak3k';
+        private $username = 'vf6wblmltrr7g6gh';
+        private $password = 'w0n4bshsd6j1ybzh';
         private $conn;
 
         public function connect() {
             $this->conn = null;
             try {
-              $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->dbname, $this->username);
+              $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->dbname, $this->username, $this->password);
               $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch(PDOException $e) {
                 echo 'Connection Error: ' . $e->getMessage();
